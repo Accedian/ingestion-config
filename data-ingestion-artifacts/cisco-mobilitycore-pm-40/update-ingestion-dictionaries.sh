49cfbd8e9487a21158c9793cac21c390c8ee1f44
Mariusz <mawilk@cisco.com>
2026-02-12 09:43:54 +0100

    cleanup

--- data-ingestion-artifacts/cisco-mobilitycore-pm-40/update-ingestion-dictionaries.sh
+++ data-ingestion-artifacts/cisco-mobilitycore-pm-40/update-ingestion-dictionaries.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 export BASE_URL="https://pca.kajar.npav.accedian.net"
-#export AUTHORIZATION_HEADER="Bearer your-token-here"
-export AUTHORIZATION_HEADER="Bearer qI5TsYU1AJ7AEm4affRBvT8TVeYMcZ_C8EYwpBM3aFONuIR_TyBjyTZJUD3GIRZuBsiDxeFpEnDzde0bZfTHbDfS8_euxUnUdvmSQpJF"
+export AUTHORIZATION_HEADER="Bearer your-token-here"
 export DICTIONARIES_PATH="./ingestion-artifacts"
 export INSECURE_SKIP_VERIFY="true"  # for self-signed certs
 
